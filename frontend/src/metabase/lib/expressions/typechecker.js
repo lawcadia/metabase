6ddb9fc39312a148210501643e0d7e55aac181a9
Ariya Hidayat <ariya@metabase.com>
2020-12-30 16:30:23 -0800

    Expression post-parsing: infer segment vs dimension
    Use the type checker for dynamic resolving of an identifier, i.e. to infer
    whether it represents a dimension or a segment, based on the type
    constraints in the sub-expression.
    This also eliminates the flawed type-based resolving which happens
    during the parsing time.

--- frontend/src/metabase/lib/expressions/typechecker.js
+++ frontend/src/metabase/lib/expressions/typechecker.js
@@ -47,18 +47,15 @@ export function typeCheck(cst, startRule) {
       return super.metricExpression(ctx);
     }
 
-    segmentExpression(ctx) {
-      const type = this.typeStack[0];
-      if (type !== "boolean") {
-        throw new Error("Incorrect type for segment");
-      }
-      return super.segmentExpression(ctx);
-    }
-
     dimensionExpression(ctx) {
       const type = this.typeStack[0];
-      if (type === "boolean" || type === "aggregation") {
-        throw new Error("Incorrect type for dimension");
+      if (type === "boolean") {
+        ctx.resolveAs = "segment";
+      } else {
+        ctx.resolveAs = "dimension";
+        if (type === "aggregation") {
+          throw new Error("Incorrect type for dimension");
+        }
       }
       return super.dimensionExpression(ctx);
     }
